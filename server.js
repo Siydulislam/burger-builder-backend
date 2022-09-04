fda1e9fcfa791b4f5fc2dbfaa675c3e930321e2b
Siydulislam <siydulislam333@gmail.com>
2022-09-04 20:02:38 +0600

    setting up project - burger builder

--- server.js
+++ server.js
@@ -3,19 +3,16 @@ dotenv.config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_SERVER, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_SERVER)
     .then(() => {
-        console.log('connected')
+        console.log('connected to mongodb!');
     })
     .catch(err => {
-        console.log('connection failed')
+        console.log('connection failed!');
     });
 
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})
+});

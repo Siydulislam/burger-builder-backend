c8631a374b8927e5683712a8eac05a555b1afabf
Siydulislam <siydulislam333@gmail.com>
2022-09-04 20:14:58 +0600

    creating user schema

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const { Schema, model } = require('mongoose');
+
+const userSchema = Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024,
+    }
+});
+
+userSchema.methods.generateJWT = function () {
+    const token = jwt.sign({ _id: this.id, email: this.email }, process.env.JWT_SECRET_KEY, { expiresIn: "3h" });
+    return token;
+}
+
+module.exports.User = model('User', userSchema);

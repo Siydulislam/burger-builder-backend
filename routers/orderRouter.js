8c32c7be3014d9e914dff9a15b491662e753ecfc
Siydulislam <siydulislam333@gmail.com>
2022-09-07 07:59:51 +0600

    new order and order list

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const { Order } = require('../models/order');
+const authorize = require('../middlewares/authorize');
 const router = express.Router();
+
+const newOrder = async (req, res) => {
+    const order = new Order(req.body);
+    try {
+        await order.save();
+        return res.status(201).send("Order placed successfully!");
+    } catch (error) {
+        return res.status(400).send("Sorry! Something went wrong!");
+    }
+}
+
+const orderList = async (req, res) => {
+    const orders = await Order.find({ userId: req.user._id }).sort({ orderTime: -1 });
+    res.send(orders);
+}
+
+router.route('/')
+    .get(authorize, orderList)
+    .post(authorize, newOrder)
+
+module.exports = router;
